4eeceeec08a3495df4c4d5bce09d8874099c5d97
azu <azuciao@gmail.com>
2014-11-30 01:43:31 +0900

    refactor(test): fix test name

--- test/ruby-translator-test.js
+++ test/ruby-translator-test.js
@@ -3,13 +3,16 @@
 var assert = require("power-assert");
 var jsdom = require('mocha-jsdom');
 var rubyTranslator = require("../lib/ruby-translator");
-describe('mocha tests', function () {
+describe('ruby-translator', function () {
     jsdom();
-    it('has document', function () {
-        var div = document.createElement("div");
-        div.innerHTML = '<p> "test.js" </p>';
-        var rubyP = rubyTranslator.translateNode(div);
-        var result = rubyP.textContent;
-        assert.equal(result, ' "test.js" ');
+    context("when contain continuing separator", function () {
+        it('should keep separator', function () {
+            var div = document.createElement("div");
+            // <space><double quote>
+            div.innerHTML = '<p> "test.js" </p>';
+            var rubyP = rubyTranslator.translateNode(div);
+            var result = rubyP.textContent;
+            assert.equal(result, ' "test.js" ');
+        });
     });
 });
